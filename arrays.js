c331a3d6fe8d5f98cb72f7191e99ba1bc95a38df
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-07-21 16:14:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
   return(temp);
 }
 
-var destructivelyAddElementToBeginningOfArray = function(array, element){
+function destructivelyAddElementToBeginningOfArray (array, element){
   array.unshift(element);
   return array
 }
