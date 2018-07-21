37d7784da80b807f3b338f59a4fd870a0958c5ea
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-07-21 16:28:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  var temp = [element, ...array];
+  let temp = [element, ...array];
   return(temp);
 }
 
